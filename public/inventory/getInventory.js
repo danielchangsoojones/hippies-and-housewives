2391beb994de3084416252bb004a0306d74f3207
Daniel Jones <danieljones@Daniels-MacBook-Pro-4.local>
2017-06-01 15:19:51 -1000

    succesfully searching through products to get inventory

--- public/inventory/getInventory.js
+++ public/inventory/getInventory.js
@@ -1,12 +1,11 @@
 var Parse = require('parse/node');
-var initializeParse = require("../../resources/initializeParse.js");
+var initializeParse = require("../resources/initializeParse.js");
 
 exports.recieveInventory = function recieveInventory(product) {
     if (product != undefined) {
-        let Inventory = require("./models/inventory.js");
+        let Inventory = require("../models/inventory.js");
         let inventory = new Inventory();
         inventory.set("product", product);
-
         inventory.save(null, {
             success: function(inventory) {},
             error: function(error) {
---
+++ public/inventory/testing/testingInventory.js
@@ -0,0 +1,11 @@
+function makeTestInventory(searchText, size) {
+    let Search = require("../search/searchProducts.js");
+    Search.searchProducts(searchText, size).then(function(products) {
+        var InventoryHelper = require("../getInventory.js");
+        InventoryHelper.recieveInventory(products[0]);
+    }, function(error) {
+        console.log(error);
+    });
+}
+
+console.log(makeTestInventory("yehyeh bottom", "S"));
