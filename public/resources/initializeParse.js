441bda3f1da2ef6c609b64a16b4c29b9a4c17a1d
Daniel Jones <danieljones@Daniels-MBP-4.attlocal.net>
2017-06-09 18:29:07 -0400

    updated initialize parse to point to both databases depending on the goal

--- public/resources/initializeParse.js
+++ public/resources/initializeParse.js
@@ -1,7 +1,25 @@
 var Parse = require('parse/node');
-var appId = "hippiesAndHousewives123SHDJ4852";
-var masterKey = "hippiesAndHousewives485738FHDJSK";
-var serverURL = "https://hippies-and-housewives.herokuapp.com/parse";
+
+//either production or development to point to the respective databases
+var development = "development";
+var production = "production";
+var configuration = development;
+
+var appId;
+var masterKey;
+var serverURL;
+
+if (configuration == production) {
+    appId = "hippiesAndHousewives123SHDJ4852";
+    masterKey = "hippiesAndHousewives485738FHDJSK";
+    serverURL = "https://hippies-and-housewives.herokuapp.com/parse";
+} else if (configuration == development) {
+    appId = "hippiesAndHousewivesDevelop489305028";
+    masterKey = "hippiesAndHousewivesDevelop4850385933";
+    serverURL = "https://hippies-and-housewives-develop.herokuapp.com/parse";
+}
+
+
 Parse.initialize(appId, masterKey);
 Parse.masterKey = masterKey;
 Parse.serverURL = serverURL;
