3b8203b60e740a0649a1e6e744c150857160d70c
Daniel <messyjones@gmail.com>
2017-06-20 06:50:46 -1000

    allocation for line items is now based upon item, not inventory

--- public/orders/allocate/allocateLineItem.js
+++ public/orders/allocate/allocateLineItem.js
@@ -3,22 +3,22 @@ var Parse = require('parse/node');
 exports.allocateLineItem = function allocateLineItem(productVariant, lineItem) {
     var promise = new Parse.Promise();
     
-    var Inventory = Parse.Object.extend("Inventory");
-    var query = new Parse.Query(Inventory);
+    var Item = Parse.Object.extend("Item");
+    var query = new Parse.Query(Item);
     query.equalTo("productVariant", productVariant);
     query.doesNotExist("lineItem");
     query.notEqualTo("isDeleted", true);
 
     query.first({
-        success: function(inventory) {
-            if (inventory == undefined) {
-                //no matching inventory found, just returning the initial lineItem
+        success: function(item) {
+            if (item == undefined) {
+                //no matching item found, just returning the initial lineItem
                 promise.resolve(lineItem);
             } else {
-                //found matching inventory, so allocate both ways
-                inventory.set("lineItem", lineItem);
-                lineItem.set("inventory", inventory);
-                promise.resolve([lineItem, inventory]);
+                //found matching item, so allocate both ways
+                item.set("lineItem", lineItem);
+                lineItem.set("item", item);
+                promise.resolve([lineItem, item]);
             }
         },
         error: function(error) {
