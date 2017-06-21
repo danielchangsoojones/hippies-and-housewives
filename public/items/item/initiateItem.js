4f3f079581699fc48c1c8ad63041d17797a97b98
Daniel <messyjones@gmail.com>
2017-06-20 18:07:39 -1000

    updated initiate to make a fake pointer

--- public/items/item/initiateItem.js
+++ public/items/item/initiateItem.js
@@ -2,11 +2,13 @@ exports.checkItemInitiation = function checkItemInitiation(item, currentUser) {
     let cut = item.get("cut");
     let sewn = item.get("sewn");
     let package = item.get("package");
+    let initiate = item.get("initiate");
 
-    if (cut != undefined || sewn != undefined || package != undefined) {
+    if ((cut != undefined || sewn != undefined || package != undefined) && initiate == undefined) {
         let Initiate = require("../../models/tracking/initiate.js");
-        let initiate = new Initiate();
-        initiate.set("user", currentUser);
+        let initiate = { __type:"Pointer", className: "Initiate"}
+        console.log(initiate);
+        // initiate.set("user", currentUser);
         item.set("initiate", initiate);
     }
 }
