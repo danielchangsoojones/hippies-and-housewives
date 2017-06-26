597cf96c36d0971ee1013eec7f1db7398ab025b7
Daniel <messyjones@gmail.com>
2017-06-26 00:52:39 -1000

    had to encode the parse objects before they get to cloud code

--- public/pickList/fetch/fetchPickList.js
+++ public/pickList/fetch/fetchPickList.js
@@ -1,9 +1,20 @@
 var Parse = require('parse/node');
 
 exports.fetchPickList = function fetchPickList() {
+    var promise = new Parse.Promise();
+
     let Pickable = require("../../models/pickable.js");
     let query = Pickable.query();
     query.include("order");
     query.include("lineItems");
-    return query.find();
+    query.find({
+        success: function(pickables) {
+            promise.resolve(Parse._encode(pickables));
+        },
+        error: function(error) {
+            promise.reject(error);
+        }
+    });
+
+    return promise;
 }
