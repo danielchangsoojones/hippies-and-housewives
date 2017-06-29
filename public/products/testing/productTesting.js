4f97b74ec3442f6ec96a3f7e292f6d9caecacf2a
Daniel <messyjones@gmail.com>
2017-06-29 11:48:50 -1000

    successfully made a function to delete duplicate products. I'm still not sure why they were happening though

--- public/products/testing/productTesting.js
+++ public/products/testing/productTesting.js
@@ -64,87 +64,167 @@ exports.getProductVariant = function getProductVariant(style, size) {
 // }
 
 //MARK: duplicate products.
-//TODO: this doesn't check to make sure that inventory is safe, so the inventory could still be bad data.
-// function getDuplicateProduct() {
-//     let alreadyUsedProductIDs = [];
-//     var duplicateProducts = [];
+function getDuplicateProduct() {
+    let alreadyUsedProductIDs = [];
+    var alreadyUsedProducts = [];
+    var duplicateProducts = [];
     
-//     let ProductType = require("../../models/productType.js");
-//     var query = ProductType.query();
-//     query.limit(10000);
+    let ProductType = require("../../models/productType.js");
+    var query = ProductType.query();
+    query.limit(10000);
 
-//     query.find({
-//       success: function(products) {
-//           for (var i = 0; i < products.length; i++) {
-//               let product = products[i];
-//               let shopifyProductID = product.get("shopifyID");
-//               if (alreadyUsedProductIDs.indexOf(shopifyProductID) == -1) {
-//                   //first time seeing this product number
-//                   alreadyUsedProductIDs.push(shopifyProductID);
-//               } else {
-//                   //it's a duplicate product number
-//                   deleteDuplicateProduct(product);
-//               }
-//           }
-//       },
-//       error: function(error) {
-//           console.log(error);
-//       }
-//     });
-// }
+    query.find({
+      success: function(products) {
+          for (var i = 0; i < products.length; i++) {
+              let product = products[i];
+              let shopifyProductID = product.get("shopifyID");
+              if (alreadyUsedProductIDs.indexOf(shopifyProductID) == -1) {
+                  //first time seeing this product number
+                  alreadyUsedProducts.push(product);
+                  alreadyUsedProductIDs.push(shopifyProductID);
+              } else {
+                  //it's a duplicate product number
+                  let alreadyUsedProduct = getAlreadyUsedProduct(alreadyUsedProducts, shopifyProductID);
+                  matchItems(alreadyUsedProduct, product).then(function(items) {
+                    deleteDuplicateProduct(product);
+                  }, function(error) {
+                      console.log(error);
+                  });
+              }
+          }
+      },
+      error: function(error) {
+          console.log(error);
+      }
+    });
+}
 
-// function deleteDuplicateProduct(duplicateProduct) {
-//         var ProductVariant = Parse.Object.extend("ProductVariant");
-//         var query = new Parse.Query(ProductVariant);
-//         query.equalTo("product", duplicateProduct);
+function getAlreadyUsedProduct(alreadyUsedProducts, targetShopifyID) {
+    for (var j = 0; j < alreadyUsedProducts.length; j++) {
+        let alreadyUsedProduct = alreadyUsedProducts[j];
+        if (alreadyUsedProduct.get("shopifyID") == targetShopifyID) {
+            return alreadyUsedProduct
+        }
+    }
+    console.log("failed to find our matching already used product");
+}
 
-//         query.find({
-//             success: function(variants) {
-//                 let objects = [variants, duplicateProduct];
-//                 var flattenedObjects = [].concat.apply([], objects);
-//                 Parse.Object.destroyAll(flattenedObjects);
-//                 matchLineItems();
-//             },
-//             error: function(error) {
-//                 console.log(error);
-//             }
-//         });
-// }
+function deleteDuplicateProduct(duplicateProduct) {
+        var ProductVariant = Parse.Object.extend("ProductVariant");
+        var query = new Parse.Query(ProductVariant);
+        query.equalTo("product", duplicateProduct);
 
-// function matchLineItems() {
-//     var LineItem = require("../../models/lineItem.js");
-//     var query = LineItem.query();
-//     query.limit(10000);
-//     query.include("productVariant");
+        query.find({
+            success: function(variants) {
+                let objects = [variants, duplicateProduct];
+                var flattenedObjects = [].concat.apply([], objects);
+                Parse.Object.destroyAll(flattenedObjects);
+                matchLineItems();
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+}
 
-//     query.find({
-//         success: function(lineItems) {
-//             for(var i = 0; i < lineItems.length; i++) {
-//                 let lineItem = lineItems[i];
-//                 if (lineItem.get("productVariant") == undefined) {
-//                      associateProductVariant(lineItem);
-//                 }
-//             }
-//         }, 
-//         error: function(error) {
-//             console.log(error);
-//         }
-//     })
-// }
+function matchLineItems() {
+    var LineItem = require("../../models/lineItem.js");
+    var query = LineItem.query();
+    query.limit(10000);
+    query.include("productVariant");
 
-// function associateProductVariant(lineItem) {
-//     var ProductVariant = Parse.Object.extend("ProductVariant");
-//     var query = new Parse.Query(ProductVariant);
+    query.find({
+        success: function(lineItems) {
+            for(var i = 0; i < lineItems.length; i++) {
+                let lineItem = lineItems[i];
+                if (lineItem.get("productVariant") == undefined) {
+                     associateProductVariant(lineItem);
+                }
+            }
+        }, 
+        error: function(error) {
+            console.log(error);
+        }
+    })
+}
+
+function associateProductVariant(lineItem) {
+    var ProductVariant = Parse.Object.extend("ProductVariant");
+    var query = new Parse.Query(ProductVariant);
     
-//     query.equalTo("shopifyVariantID", lineItem.get("shopifyVariantID"));
+    query.equalTo("shopifyVariantID", lineItem.get("shopifyVariantID"));
 
-//     query.first({
-//         success: function(variant) {
-//             lineItem.set("productVariant", variant);
-//             lineItem.save();
-//         },
-//         error: function(error) {
-//             console.log(error);
-//         }
-//     });
-// }
+    query.first({
+        success: function(variant) {
+            lineItem.set("productVariant", variant);
+            lineItem.save();
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+function matchItems(originalProductType, duplicateProductType) {
+    var promise = new Parse.Promise();
+
+    var ProductVariant = Parse.Object.extend("ProductVariant");
+    var query = new Parse.Query(ProductVariant);
+
+    query.containedIn("product", [duplicateProductType, originalProductType]);
+    query.include("product");
+    var originalProductVariants= [];
+    var duplicateProductVariants = [];
+
+    query.find({
+        success: function(productVariants) {
+            for (var i = 0; i < productVariants.length; i++) {
+                let productVariant = productVariants[i];
+                let productType = productVariant.get("product");
+                if (productType == originalProductType) {
+                    originalProductVariants.push(productType);
+                } else if (productType == duplicateProductType) {
+                    duplicateProductVariants.push(productType);
+                }
+            }
+            resetItems(originalProductVariants, duplicateProductVariants).then(function(items) {
+                promise.resolve(items);
+            }, function(error){
+                promise.reject(error);
+            });
+        },
+        error: function(error) {
+            promise.reject(error);
+        }
+    });
+
+    return promise;
+}
+
+function resetItems(originalProductVariants, duplicateProductVariants) {
+    let Item = require("../../models/item.js");
+    let query = Item.query();
+    query.containedIn("productVariant", duplicateProductVariants);
+    query.include("productVariant");
+
+    return query.find().then(function(itemsToReset) {
+        for (var i = 0; i < itemsToReset.length; i++) {
+            let replacementProductVariant = getProductVariant(originalProductVariants, itemsToReset.get("productVariant").get("shopifyVariantID"));
+            itemsToReset[i].set("productVariant", replacementProductVariant);
+        }
+
+        return Parse.Object.saveAll(itemsToReset);
+    })
+}
+
+function getProductVariant(productVariants, targetProductVariantID) {
+    for (var i = 0; i < productVariants.length; i++) {
+        let productVariant = productVariants[i];
+        let shopifyVariantID = productVariant.get("shopifyVariantID");
+        if (shopifyVariantID == targetProductVariantID) {
+            return productVariant;
+        }
+    }
+
+    console.log("could not find matching product variants");
+}
