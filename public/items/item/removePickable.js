323ad903e232e929b6e82e6f0be7d0ee34ca6c8a
Daniel <messyjones@gmail.com>
2017-06-28 22:25:53 -1000

    got rid of lineItems code

--- public/items/item/removePickable.js
+++ public/items/item/removePickable.js
@@ -14,25 +14,20 @@ exports.checkPickables = function checkPickables(lineItem) {
 function removePickable(lineItem) {
     var promise = new Parse.Promise();
 
-    if (lineItems.length > 0) {
-        let Pickable = require("../../models/pickable.js");
-        let query = Pickable.query();
-        query.equalTo("lineItems", lineItem);
-        query.find().then(function (pickables) {
-            return Parse.Object.destroyAll(pickables);
-        }).then(function (pickables) {
-            console.log("successful deletion");
-            console.log(pickables);
-            let success = true;
-            promise.resolve(success);
-        }, function (error) {
-            console.log(error);
-            promise.reject(error);
-        });
-    } else {
-        console.log("not deleted because no items length");
-        promise.resolve(true);
-    }
+    let Pickable = require("../../models/pickable.js");
+    let query = Pickable.query();
+    query.equalTo("lineItems", lineItem);
+    query.find().then(function (pickables) {
+        return Parse.Object.destroyAll(pickables);
+    }).then(function (pickables) {
+        console.log("successful deletion");
+        console.log(pickables);
+        let success = true;
+        promise.resolve(success);
+    }, function (error) {
+        console.log(error);
+        promise.reject(error);
+    });
 
     return promise;
 }
