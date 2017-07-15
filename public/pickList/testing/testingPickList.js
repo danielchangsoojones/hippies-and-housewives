051c4db3ff76117a6f9ad20136f00e37bf0afd79
Daniel <messyjones@gmail.com>
2017-07-14 20:40:09 -1000

    made a function to easily remove duplicate pickables. Still need to figure out if there is a bug in the code

--- public/pickList/testing/testingPickList.js
+++ public/pickList/testing/testingPickList.js
@@ -11,8 +11,37 @@ function fetchPickList() {
   });
 }
 
-testPickListUpdate();
 function testPickListUpdate() {
   let PickList = require("../pickList.js");
   PickList.updatePickList();
+}
+
+function removeDuplicatePickables() {
+  const Pickable = require('../../models/pickable.js');
+  let query = Pickable.query();
+  query.limit(10000);
+  query.include("order");
+  query.find({
+    success: function(pickables) {
+      parseDuplicatePickables(pickables);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+function parseDuplicatePickables(pickables) {
+  var alreadyUsedPickableIDs = [];
+  for (var i = 0; i < pickables.length; i++) {
+    let pickable = pickables[i];
+    let order = pickable.get("order");
+    if (alreadyUsedPickableIDs.indexOf(order.id) == -1) {
+      //haven't seen this pickable yet
+      alreadyUsedPickableIDs.push(order.id);
+    } else {
+      //duplicate pickable
+      pickable.destroy();
+    }
+  }
 }
