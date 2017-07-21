ee177609ef298f8b1af235f9b86ee0c67b2cbce2
Daniel <messyjones@gmail.com>
2017-07-20 15:19:42 -1000

    updated search product to be in order of createdAt date

--- public/products/search/searchProduct.js
+++ public/products/search/searchProduct.js
@@ -6,6 +6,7 @@ exports.searchProduct = function searchProduct(searchText) {
     var ProductType = require("../../models/productType.js");
     var query = ProductType.query();
     query.startsWith("lowercaseTitle", searchText.toLowerCase());
+    query.ascending("createdAt");
 
     //For some reason, if I put this query in another file and then make a promise for it, the return array to my iOS is not Parse encoded, so I can't cast it. But, if I do the query in this function, then it works fine.
     query.find({
